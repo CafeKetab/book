0634d97f3821d0e1515d6a0e1da496167ef318b5
mohammadne <mohammadne.dev@gmail.com>
2023-05-02 07:35:27 +0330

    feat: update book model

--- internal/models/book.go
+++ internal/models/book.go
@@ -1,10 +1,12 @@
 package models
 
 type Book struct {
-	Id        uint64    `json:"id"`
-	Name      string    `json:"name"`
-	Title     string    `json:"title"`
-	Authors   []Author  `json:"authors"`
-	Publisher Publisher `json:"publisher"`
-	CreatedAt string    `json:"created_at,omitempty"`
+	Id         uint64     `json:"id"`
+	Name       string     `json:"name"`
+	Title      string     `json:"title"`
+	Authors    []Author   `json:"authors"`
+	Publisher  Publisher  `json:"publisher"`
+	Language   Language   `json:"language"`
+	Categories []Category `json:"categories"`
+	CreatedAt  string     `json:"created_at,omitempty"`
 }
